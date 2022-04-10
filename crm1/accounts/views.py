71f8d8847d3ff40152a3e8ce5132b32001f136b6
Rodolfo Morales <rodolfomlphx@gmail.com>
2022-04-09 17:40:27 -0700

    added product and order models

--- crm1/accounts/views.py
+++ crm1/accounts/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return HttpResponse(request,'accounts/dashboard.html')
+    return render(request,'accounts/dashboard.html')
 def products(request):
-    return HttpResponse(request,'accounts/products.html')
+    return render(request,'accounts/products.html')
 def customer(request):
-    return HttpResponse(request, 'customer.html')
+    return render(request, 'accounts/customer.html')
